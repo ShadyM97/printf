9ce28c295e4b18f457cbf91e7adfcd1f5b7dcb96
ShadyM97 <shadymabdelkhalek@hotmail.com>
2023-04-26 01:37:58 +0000

    printf

--- _printf.c
+++ _printf.c
@@ -1,6 +1,5 @@
 #include "main.h"
 
-int get_specifier(va_list list, char src, char *dest, int index);
 /**
   * _printf - print function
   * @format: format specifiers
@@ -9,12 +8,10 @@ int get_specifier(va_list list, char src, char *dest, int index);
 int _printf(const char *format, ...)
 {
 	va_list vl;
-	int i = 0, j = 0;
-	char *buffer;
+	int i = 0, j = 0, k = 0;
+	char buffer[BUFFER_SIZE];
+	char *str;
 
-	buffer = malloc(sizeof(char) * BUFFER_SIZE);
-	if (buffer == NULL)
-		return (-1);
 	if (format == NULL)
 		return (-1);
 	va_start(vl, format);
@@ -23,7 +20,25 @@ int _printf(const char *format, ...)
 		if (format[i] == '%')
 		{
 			i++;
-			j = get_specifier(vl, format[i], &buffer[j], j) + j;
+			if (format[i] == 'c')
+			{
+				buffer[j] = va_arg(vl, int);
+				j++;
+			}
+			else if (format[i] == '%')
+			{
+				buffer[j] = '%';
+				j++;
+			}
+			else
+			{
+				str = va_arg(vl, char*);
+				for (k = 0; str[k]; k++)
+				{
+					buffer[j] = str[k];
+					j++;
+				}
+			}
 		}
 		else
 		{
@@ -33,47 +48,6 @@ int _printf(const char *format, ...)
 		i++;
 	}
 	write(1, buffer, j);
-	free(buffer);
 	va_end(vl);
 	return (j);
-}
-/**
-  * get_specifier - function to fill buffer according to the specifier
-  * @list: list
-  * @src: specifier from source input
-  * @dest: buffer
-  * @index: index of the buffer
-  * Return: final index of buffer
-  */
-int get_specifier(va_list list, char src, char *dest, int index)
-{
-	int i;
-	char *str;
-
-	switch (src)
-	{
-		case 'c':
-			{
-				dest[index] = va_arg(list, int);
-				index++;
-				break;
-			}
-		case 's':
-			{
-				str = va_arg(list, char*);
-				for (i = 0; str[i]; i++)
-				{
-					dest[index] = str[i];
-					index++;
-				}
-				break;
-			}
-		case '%':
-			{
-				dest[index] = '%';
-				index++;
-				break;
-			}
-	}
-	return (index);
 }
